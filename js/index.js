6116e6df2299091681e0982d96f3d2e9d6fccb08
kmontag <kmontag@skillz.com>
2015-08-18 14:10:05 -0400

    allow for pages to be landed upon on reload

--- js/index.js
+++ js/index.js
@@ -1,32 +1,38 @@
 $(document).ready(function() {
 
     var handle_click = function(target) {
-        console.log(target);
-        draw_page(target);
-    },
-    draw_page = function(page) {
-        $('.page.active').addClass('hidden').removeClass('active');
-        $(page).removeClass('hidden').addClass('active');
-    };
-
-    $('.navbar li').bind('click', function(e) {
-        e.preventDefault();
-        var target = $(e.target),
-            active = $('.navbar .active');
+        var active = $('.navbar .active');
 
         if(target.hasClass('fa')) {
             if (!target.parent().parent().hasClass('active')) {
                 target.parent().parent().addClass('active');
                 active.removeClass('active');
-                handle_click(target.parent().attr('href'))
+                draw_page(target.parent().attr('href'))
             }
         } else {
             if (!target.parent().hasClass('active')) {
                 target.parent().addClass('active');
                 active.removeClass('active');
-                handle_click(target.attr('href'))
+                draw_page(target.attr('href'))
             }
         }
+    },
+    draw_page = function(page) {
+        $('.page.active').addClass('hidden').removeClass('active');
+        $(page).removeClass('hidden').addClass('active');
+        window.location.hash = page;
+    };
+
+    if (window.location.hash != "") {
+        $('a[href="'+window.location.hash+'"]').parent().addClass('active');
+        draw_page(window.location.hash);
+    }
+
+    $('.navbar li').bind('click', function(e) {
+        e.preventDefault();
+        var target = $(e.target);
+
+        handle_click(target)
 
     });
 });
